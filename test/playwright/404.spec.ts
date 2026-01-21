7a9ac78334a7f5c7c0ac2644ff5556e5ef93553a
Jonathan Johnson <jon.johnson@ucsf.edu>
2026-01-21 10:55:57 -0800

    Style the 404 Page
    Switched from markdown to astro so I could lay this out better.

---
+++ test/playwright/404.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('404 Error Page', async ({ page }) => {
+  await page.goto('/missing');
+
+  await expect(page).toHaveTitle('404 Not Found | Ilios');
+  await expect(page.getByRole('heading', { level: 1 })).toContainText('Ilios');
+  await expect(page.getByRole('heading', { level: 2 })).toContainText(
+    "404: Rats! I couldn't find that.",
+  );
+});
