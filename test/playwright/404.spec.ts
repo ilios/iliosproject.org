c5f6850b910800d7bd1a6e8de8536ee4e9769043
Jonathan Johnson <jon.johnson@ucsf.edu>
2026-01-21 11:25:10 -0800

    Add a11y Tests
    Check for a11y issues using axe and fix the color contrast in our header
    so it passes.

--- test/playwright/404.spec.ts
+++ test/playwright/404.spec.ts
@@ -1,13 +1,24 @@
 import { test, expect } from '@playwright/test';
 import { takeScreenshot } from '../take-screenshot';
+import AxeBuilder from '@axe-core/playwright';
 
-test('404 Error Page', async ({ page }) => {
-  await page.goto('/missing');
+test.describe('404 Error Page', () => {
+  test('Layout', async ({ page }) => {
+    await page.goto('/missing');
 
-  await expect(page).toHaveTitle('404 Not Found | Ilios');
-  await expect(page.getByRole('heading', { level: 1 })).toContainText('Ilios');
-  await expect(page.getByRole('heading', { level: 2 })).toContainText(
-    "404: Rats! I couldn't find that.",
-  );
-  await takeScreenshot(page, '404');
+    await expect(page).toHaveTitle('404 Not Found | Ilios');
+    await expect(page.getByRole('heading', { level: 1 })).toContainText('Ilios');
+    await expect(page.getByRole('heading', { level: 2 })).toContainText(
+      "404: Rats! I couldn't find that.",
+    );
+    await takeScreenshot(page, '404');
+  });
+
+  test('should not have any a11y issues', async ({ page }) => {
+    await page.goto('/missing');
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
 });
