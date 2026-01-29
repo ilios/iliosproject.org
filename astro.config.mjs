dc19e175114fe270b9074d2946d29ca6e53df7ff
Jonathan Johnson <jon.johnson@ucsf.edu>
2026-01-28 21:47:43 -0800

    Redirect Existing URLs
    We have a variety of paths in Jekyll that lead to news items. I've
    redirected them by pattern to their new home. I also changed the name
    from posts to news to match the /news route.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astroilios.netlify.app/',
+  redirects: {
+    //redirect from the jekyll website paths to astro ones
+    '/updates/[year]/[month]/[day]/[title]': '/news/[year]-[month]-[day]-[title]',
+    '/release/[year]/[month]/[day]/[title]': '/news/[year]-[month]-[day]-[title]',
+    '/releases/[year]/[month]/[day]/[title]': '/news/[year]-[month]-[day]-[title]',
+    '/news/[year]/[month]/[day]/[title]': '/news/[year]-[month]-[day]-[title]',
+    '/status/[year]/[month]/[day]/[title]': '/news/[year]-[month]-[day]-[title]',
+  },
 });
