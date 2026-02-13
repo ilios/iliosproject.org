5f8fe2d6e8e4303c29a767815e7eacb42828ca3b
Michael Chadwick <michael.chadwick@gmail.com>
2026-02-13 14:29:24 -0800

    added rehype-external-links plugin and configured external links to open in new tab/window

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astroilios.netlify.app/',
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          properties: { target: '_blank', rel: [] },
+        },
+      ],
+    ],
+  },
 });
