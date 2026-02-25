4c12110ada46e6e441f88e371066a4f66b9261e1
Michael Chadwick <michael.chadwick@gmail.com>
2026-02-25 10:41:52 -0800

    rejiggered hosting page, and its sub-pages, so we get proper <title>s, and parking-pricing is now a full-fledged astro boy

--- test/playwright/hosting-pricing.spec.ts
+++ test/playwright/hosting-pricing.spec.ts
@@ -6,13 +6,13 @@ test.describe('Hosting Pricing Page', () => {
   test('Layout', async ({ page }) => {
     await page.goto('/hosting-pricing');
 
-    await expect(page).toHaveTitle('Pricing | Ilios');
+    await expect(page).toHaveTitle('Hosting - Pricing | Ilios');
     await expect(page.getByRole('heading', { level: 1 })).toContainText('Pricing');
     await expect(page.getByRole('heading', { level: 2 })).toContainText('We Can Host You');
 
     //I left this hardcoded on purpose so we'd have to take a peek at this pricae list at least once a year
     await expect(page.getByRole('heading', { level: 3 })).toContainText(
-      'PRICE LIST (as of 1/1/2026)',
+      'Price List (as of 1/1/2026)',
     );
     await takeScreenshot(page, 'hosting-pricing');
   });
