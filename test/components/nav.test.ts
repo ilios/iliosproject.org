454b4c3085ffc518d3c2d1f875ef69ad2b92b908
Michael Chadwick <michael.chadwick@gmail.com>
2026-03-06 12:07:30 -0800

    improved Nav component test to check if active link style works

--- test/components/nav.test.ts
+++ test/components/nav.test.ts
@@ -22,3 +22,15 @@ test('Nav', async () => {
     'https://us3.campaign-archive2.com/home/?u=845c4ebabb5b5ae7a6372c715&id=1493e3df18',
   ]);
 });
+
+test('Nav active link', async () => {
+  const container = await AstroContainer.create();
+  const htmlContent = await container.renderToString(Nav, {
+    request: new Request('http://localhost:4321/about'),
+  });
+
+  const dom = new JSDOM(htmlContent);
+  const links = dom.window.document.querySelectorAll('ul li a');
+  const linkClasses = Array.from(links).map((link) => link.getAttribute('class'));
+  expect(linkClasses).toEqual(['active', '', '', '', '', '', null]);
+});
