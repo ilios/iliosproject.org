9dbbe7ef7f435a91b79ba82952522c2b80425c98
Michael Chadwick <michael.chadwick@gmail.com>
2026-03-06 07:50:39 -0800

    added proper <h1> to homepage and demoted 'Latest News' to <h2>; if post on /home, post title now a <h3> for a11y reasons

--- test/playwright/index.spec.ts
+++ test/playwright/index.spec.ts
@@ -5,6 +5,9 @@ test('Home Page', async ({ page }) => {
   await page.goto('/');
 
   await expect(page).toHaveTitle('Home | Ilios');
-  await expect(page.getByRole('heading', { level: 1 })).toContainText('Latest News');
+  await expect(page.getByRole('heading', { level: 1 })).toContainText(
+    'Ilios - Curriculum Management from UCSF',
+  );
+  await expect(page.getByRole('heading', { level: 2 })).toContainText('Latest News');
   await takeScreenshot(page, 'home');
 });
