aad5d38f764b26d38317cbf03cd363dfa4087079
Jonathan Johnson <jon.johnson@ucsf.edu>
2026-03-13 00:08:48 -0700

    Ignore Type Error
    This is configured exactly as documented, something up with types here,
    ignoring for now.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@
 import { getViteConfig } from 'astro/config';
 
 export default getViteConfig({
+  // @ts-expect-error -- Astro's getViteConfig type does not include Vitest `test`
   test: {
     include: ['./test/components/*.test.ts'],
   },
