4da8c034c717c78d8ea819962e4b0f83ad781ac3
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2026-07-06 10:58:46 -0700

    add our own external-links plugin.

---
+++ src/lib/external-links-hast-plugin.js
@@ -0,0 +1,16 @@
+import { defineHastPlugin } from 'satteri';
+
+// Adds `target="_blank" attribute to all external links.
+// @see https://satteri.bruits.org/docs/plugins/#hast-plugins
+export default defineHastPlugin({
+  name: 'external-links',
+  element: {
+    filter: ['a'],
+    visit(node, ctx) {
+      const href = node.properties.href;
+      if (typeof href === 'string' && href.startsWith('http')) {
+        ctx.setProperty(node, 'target', '_blank');
+      }
+    },
+  },
+});
